4855ecb677831745fb2c3d15163e0aaffbcab07c
Alisher778 <internet-docs@mail.ru>
2017-10-06 13:06:22 -0400

    added show ath link button

--- src/components/parcials/nav.js
+++ src/components/parcials/nav.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import avatar from '../../assets/img/avatar.png'
 
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {showHide: 'none'}
+    
+  }
+  
+  toggleAuthLink(){
+    const val = this.state.showHide !== 'block' ? 'block' : 'none';
+    this.setState({ showHide: val });
+  }
+
   render(){
     return(
       <header>
@@ -11,7 +23,11 @@ class Nav extends Component {
             <li>Customers</li>
             <li>Tasks</li>
             <li id="user-auth">
-              <img src={avatar} alt="user-auth"/>
+              <img src={avatar} alt="user-auth" onClick={this.toggleAuthLink.bind(this)}/>
+              <ul id="auth-link" style={{display: this.state.showHide}}>
+                <li><a href="">Login</a></li>
+                <li><a href="">Sign Up</a></li>
+              </ul>
             </li>
           </ul>
         </nav>
