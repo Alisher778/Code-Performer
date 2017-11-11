06b9e73790586e92ae61e53dbb4d240765647fa4
Alisher778 <internet-docs@mail.ru>
2017-11-10 22:00:50 -0500

    saving changes

--- src/components/CodePlay.jsx
+++ src/components/CodePlay.jsx
@@ -4,71 +4,91 @@ import React, { Component } from 'react';
 class CodePlayground extends Component {
   constructor(props) {
     super(props);
-    this.state = { code1: {showTime: '2', execTime: '2.0232', msg: ''},
-                   code2: {showTime: '2', execTime: '2.0232', msg: ''}
+    this.state = { code1: {showTime: '', execTime: '', msg: '', status: '', perform: null},
+                   code2: {showTime: '', execTime: '', msg: '', status: '', perform: null}
                  }
   }
+ 
 
   evalCode() {
     var textarea1 = document.getElementById('code-val1').value;
-    var textarea2 = document.getElementById('code-val2').value;
       var excuteTime1 = 0;
       var code1Msg = '';
+      var status1 = '';
       function test1(){
         if(textarea1 === ""){
           excuteTime1 = 0;
           code1Msg = "It looks like no code added";
+          status1 = 'warning';
           return
         } else{
           try {
             var timeStart = performance.now();
             eval(textarea1)
-            textarea1.slice(0, -1)
+            // textarea1.slice(0, -1)
             var timeFinish = performance.now();
             excuteTime1 = timeFinish - timeStart;
             code1Msg = "Success";
+            status1 = 'success';
           }
           catch (e) {
             if(e){
               code1Msg = e.message;
+              status1 = 'danger';
             }
           }
         }
         
       }
       test1();
-      this.setState({ code1: {showTime: excuteTime1.toPrecision(3), execTime: excuteTime1, msg: code1Msg}});
-
+      var textarea2 = document.getElementById('code-val2').value;
       var excuteTime2 = 0;
       var code2Msg = '';
+      var status2 = '';
       function test2(){
         if(textarea2 === ""){
-          excuteTime1 = 0;
+          excuteTime2 = 0;
           code2Msg = "It looks like no code added";
+          status2 = "warning";
           return;
         } else {
           try {
             var timeStart = performance.now();
             eval(textarea2)
-            textarea2.slice(0, -1)
+            // textarea2.slice(0, -1)
             var timeFinish = performance.now();
             excuteTime2 = timeFinish - timeStart;
             code2Msg = "Success";
+            status2 = "success";
           }
           catch (e) {
             if(e){
               code2Msg = e.message;
+              status2 = "danger";
             }
           }
         }
-        
       }
       test2();
-      this.setState({ code2: {showTime: excuteTime2.toPrecision(3), execTime: excuteTime2, msg: code2Msg}});
+      var perform1 = '';
+      var perform2 = '';
+      if(excuteTime1 > excuteTime2){
+        perform1 = "warning";
+        perform2 = "success";
+      } else {
+        perform1 = "success";
+        perform2 = "warning";
+      } 
+
+      this.setState({
+                     code1: {showTime: excuteTime1.toPrecision(3), execTime: excuteTime1, msg: code1Msg, status: status1, perform: perform1},
+                     code2: {showTime: excuteTime2.toPrecision(3), execTime: excuteTime2, msg: code2Msg, status: status2, perform: perform2}
+                   });
+
   }
 
-
   render() {
+    console.log(this.state)
     return(
         <div id="code-area" className="container">
           
@@ -77,15 +97,15 @@ class CodePlayground extends Component {
             <div className="col-sm-6">
               <textarea className="form-control form-control-lg my-4" id="code-val1"></textarea>
               <ul className="list-group">
-                <li className="list-group-item list-group-item-sm"><b>Time: </b>{this.state.code1.showTime}s or <em>{this.state.code1.execTime}</em></li>
-                <li className="list-group-item list-group-item-sm"><b>Message: </b>{this.state.code1.msg}</li>
+                <li className={`list-group-item border-primary bg-${this.state.code1.perform}`}><b>Time: </b>{this.state.code1.showTime}s or <em>{this.state.code1.execTime}</em></li>
+                <li className={`list-group-item list-group-item-${this.state.code1.status}`}><b>Message: </b>{this.state.code1.msg}</li>
               </ul>
             </div>
             <div className="col-sm-6">
               <textarea className="form-control form-control-lg my-4" id="code-val2"></textarea>
               <ul className="list-group">
-                <li className="list-group-item list-group-item-sm"><b>Time: </b>{this.state.code2.showTime}s</li>
-                <li className="list-group-item list-group-item-sm"><b>Message: </b>{this.state.code2.msg}</li>
+                <li className={`list-group-item bg-${this.state.code2.perform}`}><b>Time: </b>{this.state.code2.showTime}s or <em>{this.state.code2.execTime}</em></li>
+                <li className={`list-group-item list-group-item-${this.state.code2.status}`}><b>Message: </b>{this.state.code2.msg}</li>
               </ul>
             </div>
           </div>
