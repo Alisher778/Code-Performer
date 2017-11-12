d1ec1eb485c13e11089b655d1a72ab537a7938b8
Alisher778 <internet-docs@mail.ru>
2017-11-11 19:11:15 -0500

    added footer and starting landing page

---
+++ src/components/parcials/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+  return(
+  <footer className="mt-5 text-center bg-warning py-3">&copy; <a href="#" className="font-underline">Ali Globalization</a> 2017, author <a href="#">Alisher Musurmonov</a></footer>
+  )
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './components/parcials/nav';
+import Footer from './components/parcials/footer';
 import About from './components/about';
 import CodePlayground from './components/CodePlay';
 
@@ -13,6 +14,7 @@ ReactDom.render(
       <Route exact path="/" component={ CodePlayground } />
       <Route path="/about" component={ About } />
       <Route path="/code" component={ CodePlayground } />
+      <Footer />
     </div>
   </Router>
   ,document.getElementById('root')
