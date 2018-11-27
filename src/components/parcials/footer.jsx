5a4e36cd8e6bdf7b15eeeafeb9bc0ea839f50861
Alisher778 <alisher.musurmonov89@gmail.com>
2018-11-27 21:35:07 +0500

    created build format

--- src/components/parcials/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return(
-  <footer className="text-center py-3">&copy; 2017 by <a href="https://alisher.me">Alisher Musurmonov</a> & <a href="hhtps://aliglob.com" className="font-underline">Ali Globalization</a></footer>
-  )
-}
-
-export default Footer;
