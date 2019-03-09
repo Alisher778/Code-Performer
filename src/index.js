813cae0cf75861bf50cf007d2e554d0a8fe129e9
Alisher778 <alisher.musurmonov89@gmail.com>
2019-03-09 09:50:07 +0500

    added server package and homepage url

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import LandingPage from './components/LandingPage';
 
 
 ReactDom.render(
-  <Router  basename="https://alisher778.github.io/Code-Performer">
+  <Router>
     <div>
       <Nav />
       <Route exact path="/" component={ LandingPage } />
