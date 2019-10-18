0fb5ad5618935e80a4e6ce91a401f34b031096de
Alisher778 <alisher.musurmonov89@gmail.com>
2019-10-18 22:31:41 +0500

    fixed component loading error

--- src/components/CodePlay.jsx
+++ src/components/CodePlay.jsx
@@ -4,129 +4,131 @@ import React, { Component } from 'react';
 class CodePlayground extends Component {
   constructor(props) {
     super(props);
-    this.state = { code1: {showTime: '0', execTime: '', msg: '', status: '', perform: null},
-                   code2: {showTime: '0', execTime: '', msg: '', status: '', perform: null},
-                   runTimes: 10
-                 }
+    this.state = {
+      code1: { showTime: '0', execTime: '', msg: '', status: '', perform: null },
+      code2: { showTime: '0', execTime: '', msg: '', status: '', perform: null },
+      runTimes: 10
+    }
   }
- 
+
   // Run the 1st code logic
   evalCode() {
-      const textarea1 = document.getElementById('code-val1').value;
-      let excuteTime1 = 0, code1Msg = '', status1 = '', valueList = [];
-      function test1(){
-        if(textarea1 === ""){
-          excuteTime1 = 0;
-          code1Msg = "It looks like no code added";
-          status1 = 'warning';
-          return
-        } else{
-          try {
-            const timeStart = performance.now();
-            eval(textarea1)
-            const timeFinish = performance.now();
-            valueList.push(excuteTime1 = timeFinish - timeStart);
-            code1Msg = "Success";
-            status1 = 'success';
-          }
-          catch (e) {
-            if(e){
-              code1Msg = e.message;
-              status1 = 'danger';
-            }
+    const textarea1 = document.getElementById('code-val1').value;
+    let excuteTime1 = 0, code1Msg = '', status1 = '', valueList = [];
+    function test1() {
+      if (textarea1 === "") {
+        excuteTime1 = 0;
+        code1Msg = "It looks like no code added";
+        status1 = 'warning';
+        return
+      } else {
+        try {
+          const timeStart = performance.now();
+          eval(textarea1)
+          const timeFinish = performance.now();
+          valueList.push(excuteTime1 = timeFinish - timeStart);
+          code1Msg = "Success";
+          status1 = 'success';
+        }
+        catch (e) {
+          if (e) {
+            code1Msg = e.message;
+            status1 = 'danger';
           }
         }
-        
-      }
-      let xTimes = this.state.runTimes;
-      for(let i = 0; i<xTimes; i++){
-        test1();
-      } 
-      console.log(valueList);
-      const avarage = (arr) => arr.reduce((total, num) => total + num, 0) / arr.length;
-      const avarageValue = avarage(valueList), excuteTime2 = this.state.code2.execTime;
-      let perform1 = '', perform2 = '';
-      if(avarageValue > excuteTime2){
-        perform1 = "warning";
-        perform2 = "success";
-      } else {
-        perform1 = "success";
-        perform2 = "warning";
       }
 
-      this.setState({ code1: {showTime: avarageValue.toFixed(3), execTime: avarageValue, msg: code1Msg, status: status1, perform: perform1},
-                      code2: { ...this.state.code2, perform: perform2},
-                   });
+    }
+    let xTimes = this.state.runTimes;
+    for (let i = 0; i < xTimes; i++) {
+      test1();
+    }
+    console.log(valueList);
+    const avarage = (arr) => arr.reduce((total, num) => total + num, 0) / arr.length;
+    const avarageValue = avarage(valueList), excuteTime2 = this.state.code2.execTime;
+    let perform1 = '', perform2 = '';
+    if (avarageValue > excuteTime2) {
+      perform1 = "warning";
+      perform2 = "success";
+    } else {
+      perform1 = "success";
+      perform2 = "warning";
+    }
+
+    this.setState({
+      code1: { showTime: avarageValue.toFixed(3), execTime: avarageValue, msg: code1Msg, status: status1, perform: perform1 },
+      code2: { ...this.state.code2, perform: perform2 },
+    });
 
   }
 
   // Run the 2nd code logic
   evalCode2() {
-      const textarea2 = document.getElementById('code-val2').value;
-      let excuteTime2 = 0, code2Msg = '', status2 = '', valueList = [];
-      function test2(){
-        if(textarea2 === ""){
-          excuteTime2 = 0;
-          code2Msg = "It looks like no code added";
-          status2 = "warning";
-          return;
-        } else {
-          try {
-            const timeStart = performance.now();
-            eval(textarea2)
-            const timeFinish = performance.now();
-            valueList.push(excuteTime2 = timeFinish - timeStart)
-            code2Msg = "Success";
-            status2 = "success";
-          }
-          catch (e) {
-            if(e){
-              code2Msg = e.message;
-              status2 = "danger";
-            }
+    const textarea2 = document.getElementById('code-val2').value;
+    let excuteTime2 = 0, code2Msg = '', status2 = '', valueList = [];
+    function test2() {
+      if (textarea2 === "") {
+        excuteTime2 = 0;
+        code2Msg = "It looks like no code added";
+        status2 = "warning";
+        return;
+      } else {
+        try {
+          const timeStart = performance.now();
+          eval(textarea2)
+          const timeFinish = performance.now();
+          valueList.push(excuteTime2 = timeFinish - timeStart)
+          code2Msg = "Success";
+          status2 = "success";
+        }
+        catch (e) {
+          if (e) {
+            code2Msg = e.message;
+            status2 = "danger";
           }
         }
-      } 
+      }
+    }
 
-      for(let i = 0; i<10; i++){
-        test2();
-      }            
-      let perform1 = '', perform2 = '';
-      const excuteTime1 = this.state.code1.execTime;
-      const avarage = (arr) => arr.reduce((total, num) => total + num, 0) / arr.length;
-      const avarageValue = avarage(valueList);
-      
-      if(excuteTime1 > avarageValue){
-        perform1 = "warning";
-        perform2 = "success";
-      } else {
-        perform1 = "success";
-        perform2 = "warning";
-      } 
+    for (let i = 0; i < 10; i++) {
+      test2();
+    }
+    let perform1 = '', perform2 = '';
+    const excuteTime1 = this.state.code1.execTime;
+    const avarage = (arr) => arr.reduce((total, num) => total + num, 0) / arr.length;
+    const avarageValue = avarage(valueList);
 
-      this.setState({
-                     code1: { ...this.state.code1, perform: perform1},
-                     code2: {showTime: avarageValue.toFixed(3), execTime: avarageValue, msg: code2Msg, status: status2, perform: perform2}
-                   });
+    if (excuteTime1 > avarageValue) {
+      perform1 = "warning";
+      perform2 = "success";
+    } else {
+      perform1 = "success";
+      perform2 = "warning";
+    }
+
+    this.setState({
+      code1: { ...this.state.code1, perform: perform1 },
+      code2: { showTime: avarageValue.toFixed(3), execTime: avarageValue, msg: code2Msg, status: status2, perform: perform2 }
+    });
 
   }
 
   increment() {
-    if(this.state.runTimes === 2000){
+    if (this.state.runTimes === 2000) {
       return
     }
     this.setState({ runTimes: this.state.runTimes + 10 })
-    
+
   }
 
   decrement(e) {
-    if(this.state.runTimes === 10){
+    if (this.state.runTimes === 10) {
       return
     }
     this.setState({ runTimes: this.state.runTimes - 10 })
   }
   render() {
-    return(
+    return (
       <div className="conatiner-fluid bg-light py-5">
         <div id="code-area" className="container pb-5">
           <h1 className="display-5 mt-4 text-center">Test Your Code Performance</h1>
@@ -135,7 +137,7 @@ class CodePlayground extends Component {
           </p>
           <div className="form-group" id="increment-wrapper">
             <button className="btn increment" onClick={this.increment.bind(this)}>+</button>
-              <input type="number" min="5" max="100" value={this.state.runTimes} readOnly className="form-control" id="run-times-value"/>
+            <input type="number" min="5" max="100" value={this.state.runTimes} readOnly className="form-control" id="run-times-value" />
             <button className="btn increment" onClick={this.decrement.bind(this)}>-</button>
           </div>
           <div className="row">
@@ -148,7 +150,7 @@ class CodePlayground extends Component {
               <button id="run-code" className="btn btn-success btn-lg mx-auto d-block w-50 mt-4" onClick={this.evalCode.bind(this)}>Run 1st Code</button>
 
             </div>
-            
+
             <div className="col-md-6">
               <textarea className="form-control form-control-lg my-4" id="code-val2"></textarea>
               <ul className="list-group">
@@ -163,6 +165,6 @@ class CodePlayground extends Component {
       </div>
     )
   }
-} 
+}
 
 export default CodePlayground;
